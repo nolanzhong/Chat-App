01dd76a1541f22d4c70e7641c35370b19a4965de
Nolan Zhong <nolan.zhong@duke.edu>
2024-02-04 19:44:01 -0500

    completed basic chat app

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,15 @@
-const ChatsPage = () => {
-    return <>chats...</>
-}
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
-export default ChatsPage
+const ChatsPage = (props) => {
+  return (
+    <div className="background">
+      <PrettyChatWindow
+        projectId={import.meta.env.VITE_CHAT_ENGINE_PROJECT_ID='2d3f7612-be15-4b21-822f-3f1e15110d54'}
+        username={props.user.username}
+        secret={props.user.secret}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
